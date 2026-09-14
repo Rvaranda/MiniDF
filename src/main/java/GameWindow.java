ebebc7eeca6a9049b3c22d28b2d7dffe26bd1509
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2026-09-14 19:48:56 -0300

    Initial commit

---
+++ src/main/java/GameWindow.java
@@ -0,0 +1,78 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameWindow extends JPanel implements Runnable {
+    Thread thread;
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    public static final int FPS = 60;
+    public static final int SCALE = 3;
+
+    public GameWindow() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setDoubleBuffered(true);
+        setFocusable(true);
+        setBackground(Color.black);
+    }
+
+    public void start() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    void update(double delta) {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+
+    @Override
+    public void run() {
+        long frameStart;
+        double frameMinDuration = 1000.0 / FPS;
+        double frameDurationCounter = 0.0;
+
+        double lastUpdate = System.currentTimeMillis();
+        double delta;
+
+        int fpsCounter = 0;
+        double timer = 0.0;
+
+        while (thread.isAlive()) {
+            frameStart = System.currentTimeMillis();
+
+            delta = frameStart - lastUpdate;
+
+            update(delta / 1000.0);
+            repaint();
+
+            timer += delta;
+            fpsCounter++;
+
+            if (timer >= 1000.0) {
+                //System.out.println("FPS: " + fpsCounter);
+                fpsCounter = 0;
+                timer = 0.0;
+            }
+
+            lastUpdate = System.currentTimeMillis();
+            if (lastUpdate - frameStart < frameMinDuration) {
+                try {
+                    Thread.sleep((long) (frameMinDuration - (lastUpdate - frameStart)));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("MiniDF");
+
+        GameWindow gameWindow = new GameWindow();
+        frame.setContentPane(gameWindow);
+
+        frame.pack();
+
+        gameWindow.start();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
