2f6d927a29a37efe5f4172cf637be147928dc23b
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2026-09-15 17:26:41 -0300

    Implementando melhor os tiles

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,23 +1,21 @@
-import java.awt.*;
-
 public class Tile {
-    private final Color color;
     private final int x;
     private final int y;
+    private TileType type;
+    private int visualVariant;
 
-    public Tile(int x, int y, Color color) {
+    public Tile(int x, int y, TileType type, int visualVariant) {
         this.x = x;
         this.y = y;
-        this.color = color;
+        this.type = type;
+        this.visualVariant = visualVariant;
     }
 
-    public Color getColor() {
-        return color;
+    public TileType getType() {
+        return type;
     }
 
     public int getX() { return x; }
     public int getY() { return y; }
-
-    public int getScreenX() { return x * World.TILE_SIZE; }
-    public int getScreenY() { return y * World.TILE_SIZE; }
+    public int getVisualVariant() { return visualVariant; }
 }
--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,20 +1,7 @@
-import java.awt.*;
-import java.util.Random;
-
 public class World {
     public static final int TILE_SIZE = 16;
-    public static final int WORLD_WIDTH = GameWindow.WIDTH / TILE_SIZE;
-    public static final int WORLD_HEIGHT = GameWindow.HEIGHT / TILE_SIZE;
-    Random random = new Random();
-
-    private final Color[] grassColors = new Color[] {
-            new Color(96, 194, 89),
-            new Color(111, 219, 61),
-            new Color(168, 222, 102),
-            new Color(48, 150, 42),
-            new Color(101, 191, 112),
-            new Color(99, 214, 32),
-    };
+    public static final int WORLD_WIDTH = 128;
+    public static final int WORLD_HEIGHT = 128;
 
     private Tile[] tiles = new Tile[WORLD_WIDTH * WORLD_HEIGHT];
 
@@ -22,11 +9,15 @@ public class World {
         for (int i = 0; i < tiles.length; i++) {
             int x = i % WORLD_WIDTH;
             int y = i / WORLD_WIDTH;
-            tiles[i] = new Tile(x, y, grassColors[random.nextInt(grassColors.length)]);
+            tiles[i] = new Tile(x, y, TileType.GRASS, 3);
         }
     }
 
     public Tile[] getTiles() {
         return tiles;
+    }
+
+    public Tile getTile(int x, int y) {
+        return tiles[y * WORLD_WIDTH + x];
     }
 }
