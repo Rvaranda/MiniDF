678128b5fa35cdfb9beaf3e455d75f9dfa40fca9
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2026-09-15 15:41:37 -0300

    Grama básica

--- src/main/java/GameWindow.java
+++ src/main/java/GameWindow.java
@@ -1,14 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GameWindow extends JPanel implements Runnable {
     Thread thread;
+    Random random = new Random();
     private boolean running = false;
 
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
     public static final int FPS = 60;
-    public static final int SCALE = 3;
+    //public static final int SCALE = 3;
+
+    private final Color[] grassColors = new Color[] {
+            new Color(96, 194, 89),
+            new Color(111, 219, 61),
+            new Color(168, 222, 102),
+            new Color(48, 150, 42),
+            new Color(101, 191, 112),
+            new Color(99, 214, 32),
+    };
 
     public GameWindow() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -36,6 +47,14 @@ public class GameWindow extends JPanel implements Runnable {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int size = 16;
+        for (int i = 0; i < WIDTH / size; i++) {
+            for (int j = 0; j < HEIGHT / size; j++) {
+                g2d.setColor(grassColors[random.nextInt(grassColors.length)]);
+                g2d.fillRect(i*size, j*size, size, size);
+            }
+        }
     }
 
     @Override
